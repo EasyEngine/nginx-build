12beed4e20b1a76448653ee44f260167c510654a
Mitesh Shah <Mr.Miteshah@gmail.com>
2014-10-31 14:44:51 +0530

    ngx_pagespeed 1.9.32.2 update

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -122,7 +122,7 @@ git clone git://github.com/yaoweibin/ngx_http_substitutions_filter_module.git \
 || ppa_error "Unable to clone ngx_http_substitutions_filter_module repo, exit status = " $?
 
 ppa_lib_echo "13/14 ngx_pagespeed"
-NPS_VERSION=1.8.31.4
+NPS_VERSION=1.9.32.2
 wget https://github.com/pagespeed/ngx_pagespeed/archive/release-${NPS_VERSION}-beta.tar.gz
 tar -zxvf release-${NPS_VERSION}-beta.tar.gz
 mv ngx_pagespeed-release-${NPS_VERSION}-beta  ngx_pagespeed
